4f98320e6f3a262c5082d5e894b877ae00cfd3f0
x2009again <x2009again@126.com>
2020-05-11 22:05:08 +0800

    获取节假日接口增加超时时间1s，本地节假日数据增加到2020年

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # calendar.crx
-谷歌插件，日历，可查看农历、可设置大小周
+中国节假日日历插件, 自动更新2018年后的节假日，应chrome上网友要求增加了周数显示。
 
-* 引入该插件：
-
-  1. 下载源码
-
-  2. 谷歌浏览器 -> 扩展程序 -> 开发者模式 -> 加载已解压的扩展程序
+Firefox安装地址：https://addons.mozilla.org/zh-CN/firefox/addon/%E8%8A%82%E5%81%87%E6%97%A5%E6%97%A5%E5%8E%86/ 
+Chrome安装地址：https://chrome.google.com/webstore/detail/%E8%8A%82%E5%81%87%E6%97%A5%E6%97%A5%E5%8E%86/nbnocjjjcabknnadklnobhnlnlbgcfnj
 
 
-* 效果图（[查看示例效果](https://gyxing.github.io/calendar.crx/index.html)）
+* 效果图（[查看示例效果](https://x2009again.github.io/HolidayCalendar/)）
 
-  ![img](./images/calendar.png)
+  ![img](./images/HolidayCalendar.png)
 
+参考：https://github.com/gyxing/calendar.crx
+感谢提莫的神秘商店提供的免费节假日api：http://timor.tech/api/holiday/
 
--- js/index.js
+++ js/index.js
@@ -1,40 +1,139 @@
-var bigWeeks = {
-    2018: {
-        4: [14, 21],
-        5: [5, 19],
-        6: [2, 23],
-        7: [7, 21],
-        8: [4, 18],
-        9: [1, 15]
-    }
-};
-
 $(function () {
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
 
-    var $list = $('#tb-list'), $curMonth = $('#cur-month');
+    var $list = $('#tb-list');//, $curMonth = $('#cur-month');
+    var $curYear = $("#curYear");//获取年下拉控件
+    var $curMonth = $("#curMonth");//获取月下拉控件
     var curDate = new Date(), now = new Date();
+    var curYearHoliday;
+    initSelect();//初始化下拉
+    initBtn();//初始化按钮
 
-    if(navigator.onLine) {
-        // 更新大小周数据
-        $.getJSON('https://gyxing.github.io/otherpage/static/weeks.json', function(ret) {
-            bigWeeks = ret.bigweeks;
-            window.LunarCalendar.setWorktime(ret.worktime);
-            localStorage.setItem('_ret_data', JSON.stringify(ret));
-            setMonth(curDate);
+    function getHoliday(year) {
+        return new Promise(function (resolve, reject) {
+            var result = false;
+            if (window.HolidayCalendar.hasWorktime(year)) {
+                result = true;
+                resolve("已经有节假日数据了");
+            }
+            else {
+                console.log("调用接口");
+                axios({
+                    method: 'get',
+                    url: 'http://timor.tech/api/holiday/year/' + year,
+                    timeout: 1000
+                })
+                    .then(function (data) {
+                        console.log(data);
+                        if (data.code == 0) {
+                            var worktime = {};
+                            worktime["y" + curDate.getFullYear()] = {};
+                            var hasData = false;
+                            Object.keys(data.holiday).forEach(function (key) {
+                                hasData = true;
+                                if (data.holiday[key].holiday == true)//true表示是节假日，false表示是调休
+                                {
+                                    worktime["y" + curDate.getFullYear()]["d" + key.replace("-", "")] = 2;
+
+                                } else {
+                                    worktime["y" + curDate.getFullYear()]["d" + key.replace("-", "")] = 1;
+                                }
+                            });
+                            if (hasData) {
+                                window.HolidayCalendar.setWorktime(worktime);//0无特殊安排，1工作，2放假
+                                curYearHoliday = worktime;
+                            }
+                            result = true;
+                            resolve("获取节假日数据成功");
+                        }
+                        else {
+                            result = false;
+                            reject("获取节假日信息出错，请联系开发人员处理");
+                            console.log("错误1");
+
+                        }
+                    })
+                    .catch(function (error) {
+                        debugger;
+                        console.log(error);
+                        result = false;
+                        reject("获取节假日信息出错，请联系开发人员处理");
+                        console.log("错误2");
+                    });
+                // $.ajax({
+                //     url: 'http://timor.tech/api/holiday/year/' + year,
+                //     dataType: "json",
+                //     timeout: 1000,
+                //     async: false,
+                //     success: function (data) {
+                //         if (data.code == 0) {
+                //             var worktime = {};
+                //             worktime["y" + curDate.getFullYear()] = {};
+                //             var hasData = false;
+                //             Object.keys(data.holiday).forEach(function (key) {
+                //                 hasData = true;
+                //                 if (data.holiday[key].holiday == true)//true表示是节假日，false表示是调休
+                //                 {
+                //                     worktime["y" + curDate.getFullYear()]["d" + key.replace("-", "")] = 2;
+
+                //                 } else {
+                //                     worktime["y" + curDate.getFullYear()]["d" + key.replace("-", "")] = 1;
+                //                 }
+                //             });
+                //             if (hasData) {
+                //                 window.HolidayCalendar.setWorktime(worktime);//0无特殊安排，1工作，2放假
+                //                 curYearHoliday = worktime;
+                //             }
+                //             result = true;
+                //         }
+                //         else {
+                //             result = false;
+
+                //         }
+                //     },
+                //     error: function () {
+                //         result = false;
+                //     }
+                // });
+            }
+            // return result;
         });
+    }
+    if (navigator.onLine) {
+        getHoliday(curDate.getFullYear()).then((message) => {
+            localStorage.setItem('_HolidayCalendar_data', JSON.stringify(curYearHoliday));
+            // setMonth(curDate.getFullYear(), curDate.getMonth());
+        }).catch((error) => {
+            alert(error);
+        });
+        // if (getHoliday(curDate.getFullYear())) {
+        //     localStorage.setItem('_HolidayCalendar_data', JSON.stringify(curYearHoliday));
+        //     setMonth(curDate.getFullYear(), curDate.getMonth());
+        // }
+        // else {
+        //     alert("获取节假日信息出错，请联系开发人员处理！");
+        // }
+
     }
 
     // 从缓存获取数据
-    var cacheData = JSON.parse(localStorage.getItem('_ret_data'));
-    if(cacheData) {
-        bigWeeks = cacheData.bigweeks;
-        window.LunarCalendar.setWorktime(cacheData.worktime);
+    if (localStorage.getItem('_HolidayCalendar_data') != "undefined") {
+        var cacheData = JSON.parse(localStorage.getItem('_HolidayCalendar_data'));
+        if (cacheData) {
+            // bigWeeks = cacheData.bigweeks;
+            window.HolidayCalendar.setWorktime(cacheData);
+        }
     }
-    setMonth(curDate);
+
+    //初始化当前月面板
+    setMonth(curDate.getFullYear(), curDate.getMonth(), true);
     $list.fadeIn();
 
     // 设置当前日期信息
-    var cur_info = window.LunarCalendar.solarToLunar(now.getFullYear(), now.getMonth()+1, now.getDate());
+    var cur_info = window.HolidayCalendar.solarToLunar(now.getFullYear(), now.getMonth() + 1, now.getDate());
     // 节日
     var jieri = [];
     cur_info.term && jieri.push(cur_info.term);
@@ -48,30 +147,86 @@ $(function () {
 
     $('#cur-info').html(cur_html);
 
-
-    $('.btn-pre').on('click', function () {
-        curDate = new Date(curDate.getFullYear(), curDate.getMonth() - 1, 1);
-        setMonth(curDate)
-    });
-
-    $('.btn-next').on('click', function () {
-        curDate = new Date(curDate.getFullYear(), curDate.getMonth() + 1, 1);
-        setMonth(curDate)
-    });
-
-    $('#cur-info').on('click', function() {
+    $('#cur-info').on('click', function () {
         curDate = now;
-        setMonth(now);
+        setMonth(now.getFullYear(), now.getMonth());
     });
 
-    function setMonth(time) {
-        $curMonth.text(time.getFullYear() + '年' + (time.getMonth() + 1) + '月');
+    //设置月面板
+    function setMonth(year, month, hasHoliday) {
+        if (!hasHoliday) {
+            hasHoliday = false;
+        }
+        $curYear.val(year);
+        $curMonth.val(month + 1);
+        if (!hasHoliday && (year <= now.getFullYear() + 1)) {
+            getHoliday(year).then((message) => {
 
-        var curWeeks = getWeeks(time.getFullYear(), time.getMonth());
-        setList(curWeeks);
+            }).catch((error) => {
+                alert(error);
+            });
+            // if (getHoliday(year)) {
+            // }
+            // else {
+            //     alert("获取节假日信息出错，请联系开发人员处理！");
+            // }
+        }
+        var curWeeks = getWeeks(year, month);//划分一个月中的周期
+        //设置月历面板列表
+        setList(curWeeks, year);
+    }
+    //初始化下拉
+    function initSelect() {
+        $curYear.empty();
+        for (i = 1900; i < 2101; i++) {
+            $curYear.append("<option value=" + i + ">" + i + "</option>");
+        }
+        $curYear.change(yearChange);
+        $curMonth.empty();
+        for (j = 1; j < 13; j++) {
+            $curMonth.append("<option value=" + j + ">" + j + "</option>");
+        }
+        $curMonth.change(monthChange);
+    }
+    //修改年
+    function yearChange() {
+        curDate = new Date($(event.currentTarget).val(), $curMonth.val() - 1, 1);
+        setMonth(curDate.getFullYear(), curDate.getMonth());
+    }
+    //修改月
+    function monthChange() {
+        curDate = new Date($curYear.val(), $(event.currentTarget).val() - 1, 1);
+        setMonth(curDate.getFullYear(), curDate.getMonth());
+    }
+    //初始化上、下一年，上、下一月，返回今天点击事件
+    function initBtn() {
+        $("#preYear").click(function () {
+            curDate = new Date(curDate.getFullYear() - 1, curDate.getMonth(), 1);
+            setMonth(curDate.getFullYear(), curDate.getMonth())
+        });
+
+        $("#nextYear").click(function () {
+            curDate = new Date(curDate.getFullYear() + 1, curDate.getMonth(), 1);
+            setMonth(curDate.getFullYear(), curDate.getMonth());
+        });
+
+        $("#preMonth").click(function () {
+            curDate = new Date(curDate.getFullYear(), curDate.getMonth() - 1, 1);
+            setMonth(curDate.getFullYear(), curDate.getMonth())
+        });
+
+        $("#nextMonth").click(function () {
+            curDate = new Date(curDate.getFullYear(), curDate.getMonth() + 1, 1);
+            setMonth(curDate.getFullYear(), curDate.getMonth())
+        });
+        $("#btnToday").click(function () {
+            curDate = new Date(now.getFullYear(), now.getMonth(), 1);
+            setMonth(curDate.getFullYear(), curDate.getMonth())
+        });
     }
 
-    function setList(weeks) {
+
+    function setList(weeks, year) {
         $list.empty();
         for (var i = 0; i < weeks.length; i++) {
             var week = weeks[i];
@@ -83,48 +238,44 @@ $(function () {
                     var firstDay = d.getDay();  // 第一天
                     for (var m = 0; m < firstDay; m++) {
                         var pcd = new Date(d.getFullYear(), d.getMonth(), d.getDate() - firstDay + m);
-                        _setDayInfo(tds, pcd, 'disabled');
+                        _setDayInfo(tds, pcd, year, 'disabled');
                     }
                 }
-                _setDayInfo(tds, d);
+                _setDayInfo(tds, d, year, null);
             }
             if (tds.length < 7) {
                 var dd = new Date(week[week.length - 1]);   // 最后一天
                 var len = 7 - tds.length;
                 for (var k = 0; k < len; k++) {
                     var lcd = new Date(dd.getFullYear(), dd.getMonth(), dd.getDate() + k + 1);
-                    _setDayInfo(tds, lcd, 'disabled')
+                    _setDayInfo(tds, lcd, year, 'disabled')
                 }
             }
             $list.append('<div class="table">' + tds.join('') + '</div>');
         }
     }
 
-    function _setDayInfo(tds, d, extraCss) {
+    function _setDayInfo(tds, d, year, extraCss) {
         var cls = [];
         // 当前日期
         if (d.getFullYear() == now.getFullYear() && d.getMonth() == now.getMonth() && d.getDate() == now.getDate()) {
             cls.push('cur');
         }
-        if (d.getDay() == 6 || d.getDay() == 0) {
-            // 判断是否是大周
-            if (bigWeeks[d.getFullYear()] && bigWeeks[d.getFullYear()][d.getMonth() + 1]) {
-                bigWeeks[d.getFullYear()][d.getMonth() + 1].indexOf(d.getDate()) != -1 && cls.push('big')
-            }
-        }
         var spans = '<p>' + d.getDate() + '</p>';
+        d.getDay() == 0 && cls.push('weeknum') && (spans = spans + "<span>" + getWeekNum(d, year) + "</span>");
+
         // 农历信息
-        var nongLi = window.LunarCalendar.solarToLunar(d.getFullYear(), d.getMonth() + 1, d.getDate());
-        if(nongLi.worktime == 1) {
+        var nongLi = window.HolidayCalendar.solarToLunar(d.getFullYear(), d.getMonth() + 1, d.getDate());
+        if (nongLi.worktime == 1) {
             // 补班
             cls.push('work');
         } else if (nongLi.worktime == 2) {
             // 放假
             cls.push('holiday');
         }
-        if(extraCss) { cls.push(extraCss) }
+        if (extraCss) { cls.push(extraCss) }
         var cls2 = nongLi.term || nongLi.lunarFestival || nongLi.solarFestival ? 'red' : '';
-        spans += '<p class="' + cls2 + '">' + (nongLi.lunarFestival || nongLi.solarFestival || nongLi.term || (nongLi.lunarDayName == '初一' ? nongLi.lunarMonthName : nongLi.lunarDayName)) + '</p>'
+        spans += '<p class="' + cls2 + '">' + (nongLi.lunarFestival || nongLi.solarFestival || nongLi.term || (nongLi.lunarDayName == '初一' ? nongLi.lunarMonthName : nongLi.lunarDayName)) + '</p>';
         tds.push('<div class="' + cls.join(' ') + '">' + spans + '</div>');
     }
 
@@ -148,5 +299,19 @@ $(function () {
         }
         return res;
     }
+
+    function getWeekNum(date, year) {
+        var currentyear = $curYear.val();
+        // debugger;
+        var dd = year != date.getFullYear() ? 1 : (dateiff(date, new Date(date.getFullYear(), 0, 1)) + new Date(year, 0, 1).getDay() + 1) / 7;
+        return Math.ceil(dd);
+    }
+    function dateiff(sDate1, sDate2) {
+        var dateSpan,
+            iDays;
+        dateSpan = sDate1.getTime() - sDate2.getTime();
+        iDays = Math.floor(dateSpan / (24 * 60 * 60 * 1000));
+        return iDays
+    };
 
 });
